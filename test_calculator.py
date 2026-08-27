240ecb8ec6a3cbd955cac0e9277dce06c17618e5
Srevalli <sreevallikandala18@gmail.com>
2026-08-27 15:37:06 +0530

    add variable at addition

--- test_calculator.py
+++ test_calculator.py
@@ -3,7 +3,7 @@ import calculator
 class TestCalculator(unittest.TestCase):
     def test_add(self):
         self.assertEqual(calculator.add(2,3),5)
-        self.assertEqual(calculator.add(-1,1),0)
+        self.assertEqual(calculator.add(-1,+1),0)
         self.assertEqual(calculator.add(0,0),0)
     def test_subtract(self):
         self.assertEqual(calculator.subtract(10,5),5)
